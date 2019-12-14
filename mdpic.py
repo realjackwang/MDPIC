1977505b7cfb33ed88fe1011a168ecb2d82662c6
skycity233 <544907049@qq.com>
2019-12-14 16:55:13 +0800

    1.fixed the uncertain problem caused by git pull 2.add new status notification 3.change the image name by time to avoid bugs 4.add the changeable hot-key config

--- mdpic.py
+++ mdpic.py
@@ -13,7 +13,9 @@ import os
 import wx
 import wx.adv
 import yaml
-from threading import Thread
+import datetime
+
+from threading import Thread, Timer
 from PIL import ImageGrab
 from pynput import keyboard
 from git import Repo
@@ -22,71 +24,119 @@ from pyperclip import copy
 current_key_pressed = []  # 当前按下的按键
 github_username = ''  # 填入你的github的用户名
 repository_name = ''
-
+keycode = {}
+hotkey = ''
 repo = Repo(os.getcwd())
 git = repo.git
 listener = None
 
+
+def write_keycode():
+    # key code for function keys
+    for i in range(1, 13):
+        keycode['f' + str(i)] = 111 + i
+    # key code for letter keys
+    for i in range(26):
+        keycode[chr(97 + i)] = 65 + i
+    keycode['ctrl'] = 162
+    keycode['alt'] = 164
+
+
 def init():
+    write_keycode()
     with open('config.yml', 'r', encoding='utf-8') as f:
         cont = f.read()
         config = yaml.load(cont, Loader=yaml.BaseLoader)
         try:
-            global github_username, repository_name
+            global github_username, repository_name, hotkey
             github_username = config['github_username']
             repository_name = config['repository_name']
-        except:
+            hotkey = config['hotkey'].lower().split('+')
+            for i in range(len(hotkey)):
+                hotkey[i] = keycode[hotkey[i]]
+        except KeyError:
             pass
 
 
-def savepic():
+def savepic(window):
     try:
         im = ImageGrab.grabclipboard()
-        filename = len(os.listdir('images'))
+        filename = '{0:%Y%m%d%H%M%S%f}'.format(datetime.datetime.now())
         im.save(r'images/image_' + str(filename) + '.png')
-        ThreadUpload('images\\image_' + str(filename) + '.png')
+        ThreadUpload('images\\image_' + str(filename) + '.png', window)
         copy(
             'https://raw.githubusercontent.com/' + github_username + '/' + repository_name + '/master/images/image_' + str(
                 filename) + '.png')
     except:
-        pass
+        wx.CallAfter(window.Failed)
 
 
-def on_release(key):
-    if 'Key.ctrl_l' in current_key_pressed and "'m'" in current_key_pressed:
-        savepic()
-    if str(key) in current_key_pressed:
-        current_key_pressed.remove(str(key))
-
-
-def on_press(key):
-    if str(key) not in current_key_pressed and str(key) != 'Key.f1':
-        current_key_pressed.append(str(key))
-
+def is_pressed():
+    for key in hotkey:
+        if key not in current_key_pressed:
+            return False
+    return True
 
 
 class ThreadKey(Thread):
-    def __init__(self):
+    def __init__(self, window):
         Thread.__init__(self)
+        self.window = window
         self.start()  # start the thread
 
     def run(self):
         global listener
-        listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+        listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
         listener.start()
 
+    def on_release(self, key):
+        try:
+            if key.value.vk in current_key_pressed:
+                current_key_pressed.remove(key.value.vk)
+        except AttributeError:
+            if key.vk in current_key_pressed:
+                current_key_pressed.remove(key.vk)
+
+    def on_press(self, key):
+        try:
+            if key.value.vk not in current_key_pressed:
+                current_key_pressed.append(key.value.vk)
+        except AttributeError:
+            if key.vk not in current_key_pressed:
+                current_key_pressed.append(key.vk)
+
+        if is_pressed():
+            wx.CallAfter(self.window.Uploading)
+            savepic(self.window)
 
 
 class ThreadUpload(Thread):
-    def __init__(self, filename):
+    def __init__(self, filename, window):
         Thread.__init__(self)
         self.filename = filename
+        self.window = window
         self.start()  # start the thread
 
     def run(self):
-        git.add(os.path.join(os.getcwd(), self.filename))
-        git.commit('-m', 'Add ' + self.filename)
-        git.push()
+        try:
+            git.pull()
+            git.add(os.path.join(os.getcwd(), self.filename))
+            git.commit('-m', 'Add ' + self.filename)
+            git.push()
+            wx.CallAfter(self.window.UploadSuccess)
+        except:
+            wx.CallAfter(self.window.Uploading)
+
+
+class Trans(wx.Frame):
+    def __init__(self, parent, title=' ', size=(700, 500)):
+        wx.Frame.__init__(self, parent, title=title, size=size, style=wx.STAY_ON_TOP)
+
+        label_user = wx.StaticText(self, -1, title, pos=(0, 0))
+
+        # self.Text = wx.TextCtrl(self, size=(700, 500))
+        # self.Text.SetBackgroundColour('Black'), self.Text.SetForegroundColour('Steel Blue')
+        self.SetTransparent(100)  # 设置透明
 
 
 class MyTaskBarIcon(wx.adv.TaskBarIcon):
@@ -98,8 +148,8 @@ class MyTaskBarIcon(wx.adv.TaskBarIcon):
 
     def __init__(self):
         wx.adv.TaskBarIcon.__init__(self)
-        self.SetIcon(wx.Icon(self.ICON), self.TITLE)  # 设置图标和标题
 
+        self.SetIcon(wx.Icon(self.ICON), self.TITLE)  # 设置图标和标题
         self.Bind(wx.EVT_MENU, self.onAbout, id=self.ID_ABOUT)  # 绑定“关于”选项的点击事件
         self.Bind(wx.EVT_MENU, self.onExit, id=self.ID_EXIT)  # 绑定“退出”选项的点击事件
         self.Bind(wx.EVT_MENU, self.onShowWeb, id=self.ID_SHOW_WEB)  # 绑定“显示页面”选项的点击事件
@@ -107,6 +157,20 @@ class MyTaskBarIcon(wx.adv.TaskBarIcon):
         if github_username == '' or repository_name == '':
             wx.MessageBox('config.yml is not complete yet, please fill it', "Warning")
             wx.Exit()
+
+        self.frame2 = Trans(parent=None, title='上传中', size=(50, 20))
+        self.frame2.Center()
+        self.frame2.Show(False)
+
+        self.frame3 = Trans(parent=None, title='上传成功', size=(50, 20))
+        self.frame3.Center()
+        self.frame3.Show(False)
+
+        self.frame4 = Trans(parent=None, title='上传失败', size=(50, 20))
+        self.frame4.Center()
+        self.frame4.Show(False)
+
+        ThreadKey(self)
 
     # “关于”选项的事件处理器
     def onAbout(self, event):
@@ -135,17 +199,57 @@ class MyTaskBarIcon(wx.adv.TaskBarIcon):
                 ('Info', self.ID_ABOUT),
                 ('Exit', self.ID_EXIT)]
 
+    def Uploading(self):
+        pos = wx.GetMousePosition()
+        pos = (pos[0] + 15, pos[1])
+        self.frame2.SetPosition(pos)
+
+        def timestop():
+            self.frame2.Show(False)
+            timer.cancel()
+
+        self.frame2.Show(True)
+        timer = Timer(2.0, timestop)
+        timer.start()
+
+    def UploadSuccess(self):
+        pos = wx.GetMousePosition()
+        pos = (pos[0] + 15, pos[1])
+        self.frame3.SetPosition(pos)
+
+        def timestop():
+            self.frame3.Show(False)
+            timer.cancel()
+
+        self.frame3.Show(True)
+        timer = Timer(2.0, timestop)
+        timer.start()
+
+    def UploadFailed(self):
+        pos = wx.GetMousePosition()
+        pos = (pos[0] + 15, pos[1])
+        self.frame4.SetPosition(pos)
+
+        def timestop():
+            self.frame4.Show(False)
+            timer.cancel()
+
+        self.frame4.Show(True)
+        timer = Timer(2.0, timestop)
+        timer.start()
+
 
 class MyFrame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self)
         MyTaskBarIcon()  # 显示系统托盘图标
-        ThreadKey()
+
 
 
 class MyApp(wx.App):
     def OnInit(self):
         MyFrame()
+
         return True
 
 
